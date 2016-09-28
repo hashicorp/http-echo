72f7da503b1af2b5037d4d2a2c3f6f662c9426eb
Seth Vargo <sethvargo@gmail.com>
2016-09-27 18:38:39 -0700

    Change API to accept option instead

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ as an HTML page.
 The default port is 5678, but this is configurable via the `-listen` flag:
 
 ```
-http-echo -listen=:8080 hello world
+http-echo -listen=:8080 -text="hello world"
 ```
 
 Then visit http://localhost:8080/ in your browser.
--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
-	"sync"
 	"syscall"
-	"time"
 )
 
 const (
@@ -21,6 +16,7 @@ const (
 
 var (
 	listenFlag  = flag.String("listen", ":5678", "address and port to listen")
+	textFlag    = flag.String("text", "", "text to put on the webpage")
 	versionFlag = flag.Bool("version", false, "display version information")
 )
 
@@ -29,22 +25,26 @@ func main() {
 
 	// Asking for the version?
 	if *versionFlag {
-		log.Println(formattedVersion())
+		printError(formattedVersion())
 		os.Exit(0)
 	}
 
 	// Validation
+	if *textFlag == "" {
+		printError("Missing -text option!")
+		os.Exit(127)
+	}
+
 	args := flag.Args()
-	if len(args) < 1 {
-		log.Printf("Missing server contents!")
+	if len(args) > 0 {
+		printError("Extra arguments!")
 		os.Exit(127)
 	}
 
 	// Extra args get printed to the HTML page
 	mux := http.NewServeMux()
-	data := strings.Join(flag.Args(), " ")
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, html.EscapeString(data))
+		fmt.Fprint(w, *textFlag)
 	})
 
 	server, err := NewServer(*listenFlag, mux)
@@ -74,71 +74,6 @@ func main() {
 	}
 }
 
-type StoppableServer struct {
-	wg sync.WaitGroup
-
-	server   *http.Server
-	listener *net.TCPListener
-	stopCh   chan struct{}
-}
-
-func NewServer(addr string, handler http.Handler) (*StoppableServer, error) {
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
-
-	handler = loggingMux(handler)
-
-	var stoppableServer StoppableServer
-	stoppableServer.server = &http.Server{Handler: handler}
-	stoppableServer.listener = listener.(*net.TCPListener)
-	stoppableServer.stopCh = make(chan struct{}, 1)
-
-	return &stoppableServer, nil
-}
-
-func (s *StoppableServer) Start() {
-	s.wg.Add(1)
-	defer s.wg.Done()
-	s.server.Serve(s.listener)
-}
-
-func (s *StoppableServer) Accept() (net.Conn, error) {
-	for {
-		s.listener.SetDeadline(time.Now().Add(time.Second))
-
-		conn, err := s.listener.Accept()
-		select {
-		case <-s.stopCh:
-			return nil, nil
-		default:
-		}
-
-		if err != nil {
-			netErr, ok := err.(net.Error)
-
-			// If this is a timeout, then continue to wait for new connections
-			if ok && netErr.Timeout() && netErr.Temporary() {
-				continue
-			}
-		}
-
-		return conn, err
-	}
-}
-
-func (s *StoppableServer) Stop() {
-	close(s.stopCh)
-}
-
-func (s *StoppableServer) Wait() {
-	s.wg.Wait()
-}
-
-func loggingMux(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("[%s] %s\n", r.Method, r.URL)
-		h.ServeHTTP(w, r)
-	})
+func printError(s string) {
+	fmt.Fprintf(os.Stderr, "%s\n", s)
 }
